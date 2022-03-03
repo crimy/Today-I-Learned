953894d4adcc2c1af1c61b34c6917c52bd76a1e4
서승원 <97273627+crimy@users.noreply.github.com>
2022-03-03 11:38:32 +0900

    [Gold V] Title: CCW, Time: 220 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main( String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		int[][] p = new int[2][3];
+		
+		for( int i = 0 ; i < 3 ; i++ ) {
+			p[0][i] = sc.nextInt();
+			p[1][i] = sc.nextInt();
+		}
+
+		int result = ccw(p);
+		System.out.println(result);
+	}
+
+	private static int ccw(int[][] p) {
+		int ccw = ( p[0][0] * p[1][1] + p[0][1] * p[1][2] + p[0][2] * p[1][0] )
+				- ( p[0][1] * p[1][0] + p[0][2] * p[1][1] + p[0][0] * p[1][2] );
+		if ( ccw > 0 ) return 1;
+		else if ( ccw < 0 ) return -1;
+		else return 0;
+	 }
+}
