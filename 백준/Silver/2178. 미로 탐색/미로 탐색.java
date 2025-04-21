982cc1cb8872d325dd23eb7934441660ebd56316
서승원 <97273627+crimy@users.noreply.github.com>
2025-04-21 20:11:41 +0900

    [Silver I] Title: 미로 탐색, Time: 204 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        map = new int[n][m];
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+        visited = new boolean[n][m];
+        int result = bfs();
+        System.out.println(result);
+    }
+
+    public static int bfs() {
+        int result = Integer.MAX_VALUE;
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(new Position(0, 0, 1));
+        while (!queue.isEmpty()) {
+            Position current = queue.poll();
+            if (current.x == n - 1 && current.y == m - 1) {
+                result = Math.min(result, current.count);
+            }
+            for (int k = 0; k < 4; k++) {
+                int nx = current.x + dx[k];
+                int ny = current.y + dy[k];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || map[nx][ny] == 0) {
+                    continue;
+                }
+                queue.offer(new Position(nx, ny, current.count + 1));
+                visited[nx][ny] = true;
+            }
+        }
+        return result;
+    }
+
+    public static class Position {
+        int x;
+        int y;
+        int count;
+
+        public Position(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+    }
+}
